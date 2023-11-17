cefcc617d6ad378f8e7fae5bbeab1ed7f35e6857
Helen Catacora <catacorapatanahelenjazmin@gmail.com>
2023-11-16 21:10:54 -0400

    Apis 2

--- src/assets/pasteles.js
+++ src/assets/pasteles.js
@@ -3,30 +3,30 @@ export const pasteles= [
         title:"Torta de tres leches",
         available: "3 unidades",
         price: " 9",
-        imgUrl: "/T3leches.svg",
+        imgUrl: "/menu/T3leches.svg",
     },
     {
         title:"Torta de chocolate",
         available: "8 unidades",
         price: " 12",
-        imgUrl: "/Tchocolate.svg",
+        imgUrl: "/menu/Tchocolate.svg",
     },
     {
         title:"Torta de Maracuyá",
         available: "2 unidades",
         price: " 9.50",
-        imgUrl: "/Tmaracuya.svg",
+        imgUrl: "/menu/Tmaracuya.svg",
     },
     {
         title:"Torta de mil hojas",
         available: "1 unidad",
         price: " 10",
-        imgUrl: "/Tmillhojas.svg",
+        imgUrl: "/menu/Tmillhojas.svg",
     },
     {
         title:"Torta de frutilla",
         available: "Sold out",
         price: " 10",
-        imgUrl: "/Tfrutilla.svg",
+        imgUrl: "/menu/Tfrutilla.svg",
     }
 ]
