642707e8799839af6a12a50addf734445271cb46
PabloPachecoB <pablitopacheco051@gmail.com>
2023-11-23 13:58:57 -0400

    css top

--- src/components/organisms/SuccesfulOrder.jsx
+++ src/components/organisms/SuccesfulOrder.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import HeaderGo from '../molecules/HeaderGo';
 import OrderSummary from '../molecules/OrderSummary';
 import InfoButton from '../atoms/InfoButton';
+import BackButton from '../atoms/BackButton';
 import "./styles/SuccesfulOrder.css"
 
 function SuccessfulOrder() {
   
   return (
     <div className="successful-order">
+       
+      
       <HeaderGo />
       <OrderSummary />
       <InfoButton />
