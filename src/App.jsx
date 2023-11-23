00e8921e421fc69383cd35ec982907225f993225
Helen Catacora <catacorapatanahelenjazmin@gmail.com>
2023-11-23 09:12:13 -0400

    Borrando

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import './App.css'
-import Description from './components/molecules/Description'
-import Informacion from './components/molecules/Informacion'
-import ProductView from './components/molecules/ProductView'
-import ProductInformacion from './components/organisms/ProductInformacion'
 import Inicio1 from './screens/Inicio1'
 import Inicio2 from './screens/Inicio2'
 import MenuHumintas from './screens/MenuHumintas'
@@ -11,7 +7,6 @@ function App() {
 
   return (
     <>
-      <ProductInformacion />
       
       
     </>
