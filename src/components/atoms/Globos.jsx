34783e35546714f8e679a3916cfe65c4a76d01b3
PabloPachecoB <pablitopacheco051@gmail.com>
2023-11-22 20:04:12 -0400

    actualicacion

---
+++ src/components/atoms/Globos.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import "./styles/Globos.css"
+function Globos() {
+  return (
+    <div className='Globos'>
+        <img src='public/images/globos.png' className='imagen'></img>
+    </div>
+  )
+}
+export default Globos
+
+
+
+
