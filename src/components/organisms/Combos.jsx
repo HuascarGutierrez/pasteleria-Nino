246387712c9c16e4b2cad546d418fddf198bb64e
Helen Catacora <catacorapatanahelenjazmin@gmail.com>
2023-11-23 23:17:48 -0400

    carrusel?

--- src/components/organisms/Combos.jsx
+++ src/components/organisms/Combos.jsx
@@ -3,6 +3,15 @@ import CardPromo from '../molecules/CardPromo'
 import { ofertas } from '../../assets/OfertasAPI'
 import "./styles/Fliper.css"
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
 
 function Combos() {
   return (
@@ -10,13 +19,25 @@ function Combos() {
       <section className='cardfliper'>
       <h3 className='titleh3'>Combos Disponibles</h3>
       <div>
-        {
-            ofertas.map(ofer=>{
-                return(
-                <CardPromo ofertas={ofer}/>
-                )
-            })
+        <Swiper
+      slidesPerView={1}
+      centeredSlides={true}
+      spaceBetween={30}
+      grabCursor={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="mySwiper"
+    >
+      {
+          ofertas.map(ofer=>{
+            return(
+              <CardPromo ofertas={ofer}/>
+            )
+          })
         }
+    </Swiper> 
       </div>
         
       </section>
--- src/components/organisms/Ofertas.jsx
+++ src/components/organisms/Ofertas.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import "./styles/Fliper.css"
+
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
 import CardPromo from '../molecules/CardPromo'
 import { ofertas } from '../../assets/OfertasAPI'
-import "./styles/Fliper.css"
+
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+
 
 function Ofertas() {
   return (
@@ -9,13 +22,25 @@ function Ofertas() {
       <section className='cardfliper'>
         <h3 className='titleh3'>Las mejores ofertas</h3>
         <div>
-        {
-          ofertas.map(ofer=>{
-            return(
-              <CardPromo ofertas={ofer}/>
-            )
-          })
-        }
+          <Swiper
+          slidesPerView={1}
+          centeredSlides={true}
+          spaceBetween={30}
+          grabCursor={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          {
+              ofertas.map(ofer=>{
+                return(
+                  <CardPromo ofertas={ofer}/>
+                )
+              })
+          }
+        </Swiper>
         </div>
       </section>
     </>
